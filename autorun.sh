b3e0a6edf20b3107223a3a3dcb9125f024499ea9
Function Error <138248655+FunctionError@users.noreply.github.com>
2024-04-27 05:19:02 +0600

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python generate_playlist.py
+# Set LD_LIBRARY_PATH to include Python libraries
+export LD_LIBRARY_PATH="/opt/hostedtoolcache/Python/3.12.3/x64/lib:$LD_LIBRARY_PATH"
+
+# Add your commands here
+python script.py
