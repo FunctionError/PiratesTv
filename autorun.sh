3aeddfdae8c079ebd8ba7116e7eb13a3d2c35c94
Function Error <138248655+FunctionError@users.noreply.github.com>
2024-04-27 05:19:44 +0600

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Set LD_LIBRARY_PATH to include Python libraries
-export LD_LIBRARY_PATH="/opt/hostedtoolcache/Python/3.12.3/x64/lib:$LD_LIBRARY_PATH"
-
-# Add your commands here
 python script.py
